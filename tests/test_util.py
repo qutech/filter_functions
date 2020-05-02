ca9c707f112ef807c7435f0962964b603df00ce4
Tobias Hangleiter <tobias.hangleiter@rwth-aachen.de>
2020-05-02 18:55:45 +0200

    Fix test for 1df2fdc

--- tests/test_util.py
+++ tests/test_util.py
@@ -511,9 +511,9 @@ class UtilTest(testutil.TestCase):
         omega = util.get_sample_frequencies(pulse)
         self.assertAlmostEqual(omega[0], -2e2*np.pi/pulse.t[-1])
         self.assertAlmostEqual(omega[-1], 2e2*np.pi/pulse.t[-1])
-        self.assertEqual(len(omega), 200)
-        self.assertTrue((omega[:100] <= 0).all())
-        self.assertLessEqual(np.var(np.diff(np.log(omega[100:]))), 1e-16)
+        self.assertEqual(len(omega), 300)
+        self.assertTrue((omega[:150] <= 0).all())
+        self.assertLessEqual(np.var(np.diff(np.log(omega[150:]))), 1e-16)
 
         # custom args
         omega = util.get_sample_frequencies(pulse, spacing='linear',
