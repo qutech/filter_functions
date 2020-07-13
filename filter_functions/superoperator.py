Fix normalization of choi operator

filter_functions/superoperator.py
@@ -106,7 +106,7 @@ def liouville_to_choi(superoperator: ndarray, basis: _b.Basis) -> ndarray:
     .. math::
 
         \mathrm{choi}(\mathcal{S})
-            &= (\mathcal{S}\otimes\mathbb{I}) (|\Omega\rangle\langle\Omega|) \\
+            &= (\mathbb{I}\otimes\mathcal{S}) (|\Omega\rangle\langle\Omega|) \\
             &= \sum_{ij} E_{ij}\otimes\mathcal{S}(E_{ij})
 
     where :math:`|\Omega\rangle` is a maximally entangled state and
@@ -136,7 +136,7 @@ def liouville_to_choi(superoperator: ndarray, basis: _b.Basis) -> ndarray:
     path = ['einsum_path', (0, 2), (0, 1), (0, 1)]
     choi = np.einsum('iab,...jk,ki,jcd->...acbd',
                      ij, superoperator, ij_ket, basis,
-                     optimize=path).reshape(superoperator.shape) / d
+                     optimize=path).reshape(superoperator.shape)
 
     return choi
 

tests/test_superoperator.py
@@ -88,7 +88,7 @@ class SuperoperatorTest(testutil.TestCase):
             choi = superoperator.liouville_to_choi(U_sup, basis).view(ff.Basis)
 
             self.assertTrue(choi.isherm)
-            self.assertArrayAlmostEqual(np.einsum('...ii', choi), 1)
+            self.assertArrayAlmostEqual(np.einsum('...ii', choi), d)
 
             pulse = testutil.rand_pulse_sequence(d, 1)
             omega = ff.util.get_sample_frequencies(pulse)
@@ -98,7 +98,7 @@ class SuperoperatorTest(testutil.TestCase):
             choi = superoperator.liouville_to_choi(U_sup, basis).view(ff.Basis)
 
             self.assertTrue(choi.isherm)
-            self.assertAlmostEqual(np.einsum('ii', choi), 1)
+            self.assertAlmostEqual(np.einsum('ii', choi), d)
 
     def test_liouville_is_CP(self):
         def partial_transpose(A):