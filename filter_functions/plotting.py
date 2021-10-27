f825bacb1ce57b6de934eefa393039134fe9de26
Tobias Hangleiter <tobias.hangleiter@rwth-aachen.de>
2021-10-27 11:52:20 +0200

    Fix typo

--- filter_functions/plotting.py
+++ filter_functions/plotting.py
@@ -224,7 +224,7 @@ def plot_bloch_vector_evolution(
         figsize = bloch_kwargs.pop('figsize', [5, 5])
         view = bloch_kwargs.pop('view', [-60, 30])
         fig = plt.figure(figsize=figsize)
-        axes = fig.add_subplot(projection='3d', axim=view[0], elev=view[1])
+        axes = fig.add_subplot(projection='3d', azim=view[0], elev=view[1])
         b = init_bloch_sphere(fig=fig, axes=axes, **bloch_kwargs)
 
     if n_samples is None:
